b335dbb0b72e62f91a33189b546009fe3a14f5d2
lipemon1 <ramon_felipe@outlook.com>
2021-05-30 20:19:28 -0300

    Moving player around

---
+++ Assets/Progging/Gameplay/Scripts/PlayerMovement/PlayerInput.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Phrogging.PlayerMovement
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        public static Vector3 PlayerDir;
+
+        Transform _playerCamTransform;
+
+        //cache variables
+        float horizontalValue;
+        float verticalValue;
+
+        private void Awake()
+        {
+            _playerCamTransform = Camera.main.transform;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            horizontalValue = Input.GetAxis("Horizontal");
+            verticalValue = Input.GetAxis("Vertical");
+
+            //PlayerDir = new Vector3(horizontalValue, 0f, verticalValue);
+            var forward = _playerCamTransform.forward;
+            forward.y = 0f;
+
+            var right = _playerCamTransform.right;
+            right.y = 0f;
+
+            PlayerDir = forward * verticalValue + right * horizontalValue;
+        }
+    }
+}
---
+++ Assets/Progging/Gameplay/Scripts/PlayerMovement/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Phrogging.PlayerMovement
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private Transform _visualTransform;
+        CharacterController _characterController;
+
+        private void Awake()
+        {
+            _characterController = GetComponent<CharacterController>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (PlayerInput.PlayerDir == Vector3.zero) return;
+            MovePlayer(PlayerInput.PlayerDir);
+            RotatePlayer(PlayerInput.PlayerDir);
+        }
+
+        void MovePlayer(Vector3 playerDir)
+        {
+            _characterController.Move(playerDir * _moveSpeed * Time.deltaTime);
+        }
+
+        void RotatePlayer(Vector3 playerDir)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(playerDir, _visualTransform.transform.up);
+            _visualTransform.transform.rotation = Quaternion.Slerp(_visualTransform.transform.rotation, newRotation, Time.deltaTime * 8);
+        }
+    }
+}
